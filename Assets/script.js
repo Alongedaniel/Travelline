2d195424bcc9d48c8c11ab784afd60829c5ba093
Alongedaniel <alongedaniel41@gmail.com>
2022-03-29 14:11:26 +0100

    Media Queries and functionatity for header done including side bar for mobile view

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,22 @@
 let themeToggler = document.querySelector(".theme");
 let togglerCircle = document.querySelector(".circle");
+let menuIcon = document.querySelector(".menu-icon");
+let mobileNav = document.querySelector(".mobile-nav");
+let overlay = document.querySelector(".overlayer");
+let body = document.querySelector("body");
 
 themeToggler.addEventListener("click", () => {
   togglerCircle.classList.toggle("toggler");
 });
+
+let sideBar = function () {
+  menuIcon.addEventListener("click", () => {
+    mobileNav.style.transform = "translateY(0)";
+    overlay.classList.add("overlay");
+  });
+
+  overlay.addEventListener("click", () => {
+    mobileNav.style.transform = "translateY(-100vh)";
+    overlay.classList.remove("overlay");
+  });
+};
