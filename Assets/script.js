c2d41e15ddd041faa8e7529a0ebd3b809f77226c
Alongedaniel <alongedaniel41@gmail.com>
2022-03-30 17:40:33 +0100

    Added the Tour Section, Trip Section, Latest Tour Section, Our Team Section, and Our Services Section.

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,24 @@
-let themeToggler = document.querySelector(".theme");
-let togglerCircle = document.querySelector(".circle");
-let menuIcon = document.querySelector(".menu-icon");
-let mobileNav = document.querySelector(".mobile-nav");
-let overlay = document.querySelector(".overlayer");
+
+
+
 let body = document.querySelector("body");
 
-themeToggler.addEventListener("click", () => {
-  togglerCircle.classList.toggle("toggler");
-});
+let themeToggle = function () {
+  let themeToggler = document.querySelector(".theme");
+let togglerCircle = document.querySelector(".circle");
+
+  themeToggler.addEventListener("click", () => {
+    togglerCircle.classList.toggle("toggler");
+  });
+};
 
 let sideBar = function () {
+  let menuIcon = document.querySelector(".menu-icon");
+let mobileNav = document.querySelector(".mobile-nav");
+let overlay = document.querySelector(".overlayer");
+
+
+
   menuIcon.addEventListener("click", () => {
     mobileNav.style.transform = "translateY(0)";
     overlay.classList.add("overlay");
