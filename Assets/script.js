c26c29287e519775f8939d893c8b719e53d800be
Alongedaniel <alongedaniel41@gmail.com>
2022-04-01 15:55:08 +0100

    Added fixes

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,7 @@ let togglerCircle = document.querySelectorAll(".circle");
 let menuIcon = document.querySelector(".menu-icon");
 let mobileNav = document.querySelector(".mobile-nav");
 let overlay = document.querySelector(".overlayer");
+let closeIcon = document.querySelector('.close')
 
 for (let i = 0; i < themeToggler.length; i++) {
   themeToggler[i].addEventListener("click", () => {
@@ -21,4 +22,10 @@ let sideBar = function () {
     mobileNav.style.transform = "translateY(-100vh)";
     overlay.classList.remove("overlay");
   });
+  closeIcon.addEventListener("click", () => {
+    mobileNav.style.transform = "translateY(-100vh)";
+    overlay.classList.remove("overlay");
+  });
 };
+
+
